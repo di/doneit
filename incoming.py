76b625d0cdcd74fdb66fdce8c6011eb413a76d70
KevinM60 <kevinm60@yahoo.com>
2012-12-01 23:13:53 -0500

    after testing adding tasks to db

--- incoming.py
+++ incoming.py
@@ -10,18 +10,17 @@ import doneit
 def get_user(email):
     # extract email address from From field
     sender = email['From']
-    doneit.log("Got email from %s" % sender)
 
     m = re.search('<([\w@\.]+)>', sender)
     sender_address = m.group(1)
-    user_id = doneit.get_user_by_email(sender_address)['_id']
-    return user_id
+    user = doneit.get_user_by_email(sender_address)
+    return user
 
 def get_project(email):
     # lookup from user id
-    user_id = get_user(email)
-    project_id = doneit.get_by_id('users', user_id)["project_id"]
-    return project_id
+    user = get_user(email)
+    project = doneit.get_by_id('projects', user['project_id'])
+    return project
 
 def get_tasks(email):
     # parse tasks out of the payload
@@ -30,9 +29,9 @@ def get_tasks(email):
 
     task_type = 'UNSPECIFIED'
     for line in payload.splitlines():
-        m = re.search('TODO|DOING|DONE|BLOCK', line)
+        m = re.search('TODO|DOING|DONE|BLOCK', line, re.IGNORECASE)
         if(m):
-            task_type = m.group(0)
+            task_type = m.group(0).lower()
         else:
             if(task_type != 'UNSPECIFIED'):
                 m = re.search('\*(.+)', line)
@@ -42,18 +41,26 @@ def get_tasks(email):
     return tasks
 
 
+def main():
+    message = email.message_from_string(sys.stdin.read())
 
-email = email.message_from_string(sys.stdin.read())
+    user = get_user(message)
+    project = get_project(message)
+    tasks = get_tasks(message)
 
-entity = dict()
-entity['date'] = datetime.datetime.utcnow()
-entity['user_id'] = get_user(email)
-entity['project_id'] = get_project(email)
+    doneit.log("Got email from %s" % user['name'])
 
-tasks = get_tasks(email)
-for task in tasks:
-    for field in ['type', 'comment']:
-        entity[field] = task[field]
-#    doneit.log("Submit Task: %s, %s, %s, %s" % (entity['user_id'], entity['project_id'], entity['type'], entity['comment']))
-    _id = doneit.add_task(entity)
+    for task in tasks:
+        entity = dict()
+        entity['date'] = datetime.datetime.utcnow()
+        entity['user_id'] = user['_id']
+        entity['project_id'] = project["_id"]
+        for field in ['type', 'comment']:
+            entity[field] = task[field]
 
+        doneit.log("Submit Task: %s, %s, %s, %s" % (user['email'], project['name'], task['type'], task['comment']))
+        _id = doneit.add_task(entity)
+
+
+if __name__ == "__main__":
+    main()
